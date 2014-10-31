1904137abb1daa148161cc0183d59a432761d977
Russell Sanborn <russellsanborn@gmail.com>
2014-10-31 10:21:19 -0500

    updated tests to use where sections to compact the test code

--- src/test/groovy/com/rocksoft/curly/CurlySpec.groovy
+++ src/test/groovy/com/rocksoft/curly/CurlySpec.groovy
@@ -5,81 +5,39 @@ import spock.lang.Specification
 class CurlySpec extends Specification {
 
   def "Normalizes URLs"() {
-    when:
-    String url = Curly.normalizeUrl("http://www.rocksoftcode.com#/test")
+    expect:
+    Curly.normalizeUrl(url) == normalizedUrl
 
-    then:
-    url == "http://www.rocksoftcode.com/test"
-
-    when:
-    url = Curly.normalizeUrl("http://www.somespaces.com/spaced out stuff")
-
-    then:
-    url == "http://www.somespaces.com/spaced%20out%20stuff"
+    where:
+    url                                          | normalizedUrl
+    "http://www.rocksoftcode.com#/test"          | "http://www.rocksoftcode.com/test"
+    "http://www.somespaces.com/spaced out stuff" | "http://www.somespaces.com/spaced%20out%20stuff"
   }
 
   def "Gets status 'line'"() {
-    when:
-    String statusLine = Curly.readStatusLine("<html></html>\n400 text/html")
+    expect:
+    Curly.readStatusLine(response) == statusLine
 
-    then:
-    statusLine == "400 text/html"
-
-    when:
-    statusLine = Curly.readStatusLine('''<html></html>\n\n200''')
-
-    then:
-    statusLine == "200"
-
-    when:
-    statusLine = Curly.readStatusLine("200 text/html 801039")
-
-    then:
-    statusLine == "200 text/html 801039"
-
-    when:
-    statusLine = Curly.readStatusLine("200 text/html;charset=utf-8 801039")
-
-    then:
-    statusLine == "200 text/html;charset=utf-8 801039"
-
-    when:
-    statusLine = Curly.readStatusLine("301")
-
-    then:
-    statusLine == "301"
-
-    when:
-    statusLine = Curly.readStatusLine("</someXml>\n 200")
-
-    then:
-    statusLine == "200"
-
-    when:
-    statusLine = Curly.readStatusLine(null)
-
-    then:
-    statusLine == null
+    where:
+    response                             | statusLine
+    "<html></html>\n400 text/html"       | "400 text/html"
+    '''<html></html>\n\n200'''           | "200"
+    "200 text/html 801039"               | "200 text/html 801039"
+    "200 text/html;charset=utf-8 801039" | "200 text/html;charset=utf-8 801039"
+    "301"                                | "301"
+    "</someXml>\n 200"                   | "200"
+    null                                 | null
   }
 
   def "Cleans up charset on content-type"() {
-    when:
-    String statusLine = Curly.readStatusLine("200 text/html; charset=utf-8 801039")
+    expect:
+    Curly.readStatusLine(response) == statusLine
 
-    then:
-    statusLine == "200 text/html;charset=utf-8 801039"
-
-    when:
-    statusLine = Curly.readStatusLine("200 text/html;   charset=utf-8 801039")
-
-    then:
-    statusLine == "200 text/html;charset=utf-8 801039"
-
-    when:
-    statusLine = Curly.readStatusLine("200 text/foo;   charset=utf-8 801039")
-
-    then:
-    statusLine == "200 text/foo;charset=utf-8 801039"
+    where:
+    response                                | statusLine
+    "200 text/html; charset=utf-8 801039"   | "200 text/html;charset=utf-8 801039"
+    "200 text/html;   charset=utf-8 801039" | "200 text/html;charset=utf-8 801039"
+    "200 text/foo;   charset=utf-8 801039"  | "200 text/foo;charset=utf-8 801039"
   }
 
   def "Parses a HEAD response into an object"() {
